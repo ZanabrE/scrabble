a77be063f4dee656edb9e79c9b929c7822cab9f1
G:\Git <ernesto.zanabria1979@gmail.com>
2022-10-15 14:06:47 -0400

    Modified scrabble.c

--- scrabble.c
+++ scrabble.c
@@ -5,7 +5,7 @@
 int main (void){
 
     //Array declaration.
-    int POINT = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    int POINT[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
     //Get user's input.
     char word1 = get_string("Player 1: ");
